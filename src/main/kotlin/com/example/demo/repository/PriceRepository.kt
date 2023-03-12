809e8db1e102577b1cbd83c9f97d00e0e10e6088
우준호 <noggong@gmail.com>
2023-03-12 21:53:47 +0900

    interface dataType 수정

--- src/main/kotlin/com/example/demo/repository/PriceRepository.kt
+++ src/main/kotlin/com/example/demo/repository/PriceRepository.kt
@@ -1,15 +1,11 @@
 package com.example.demo.repository
 
-import com.example.demo.document.Price
-import com.example.demo.document.LowestPriceByBrandESDto
-import com.example.demo.document.LowestPriceTerms
-import com.example.demo.document.PriceByCategoryESDto
-import java.util.Optional
+import com.example.demo.document.*
 
 interface PriceRepository {
     fun lowestPriceByCategory(): LowestPriceTerms
-    fun lowestPriceByBrand(): LowestPriceByBrandESDto
-    fun priceByCategory(category: String): PriceByCategoryESDto
+    fun lowestPriceByBrand(): BrandPriceAgg
+    fun priceByCategory(category: String): CategoryPriceAgg
     fun save(price: Price): Price
     fun findById(id: String): Price?
     fun delete(id: String): Unit
